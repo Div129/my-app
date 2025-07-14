45c17b3193fccd55047a56f505958d8094428478
Div129 <2021wb86681@wilp.bits-pilani.ac.in>
2025-07-14 22:43:05 +0530

    Update app.java

--- src/main/java/com/app.java
+++ src/main/java/com/app.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins CI!");
+        System.out.println("Hello, Jenkins CI!, This is Div");
     }
 }
